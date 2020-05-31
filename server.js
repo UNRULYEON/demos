5fef57ee44e85b97462d406f9933981ddcbba613
Amar Kisoensingh <amar@kisoensingh.sh>
2020-05-31 15:20:58 +0200

    Updated cors config

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const port = 3001;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-// app.use(cors())
+app.use(cors({
+  origin: '*'
+}))
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
